0b4b5fec831eeb5ef4c1f92a0af24220b21e4a63
Obada-PC\Obada <obada.abdalkareem@yahoo.com>
2018-03-04 17:33:02 +0200

    push last change

--- src/components/testApi.vue
+++ src/components/testApi.vue
@@ -14,20 +14,26 @@
   },
        created(){
         var data = {
-        client_id: '3e258v1i3dusowk484c4c0kcogg8swow0g0wkgc8ccc0400k04',
-        client_secret: '539764a6jw4c8c8ogggkco0c4cw4kok0g808gk4gcgksgwskkk', /* 3rfGlYVTbbuNdej0AR7eFPy927x0R4Aga7OJoJml */
+        client_id: 'demo_client', //'3rj21qm124u80k8ss408s0sw4g8w48o0884wk88o0ws008ko0o',
+        client_secret: 'secret_demo_client',// '3cbaxbya9e04k80gwog8gogo0cckkk0444800gk884w8g0kwsk', /* 3rfGlYVTbbuNdej0AR7eFPy927x0R4Aga7OJoJml */
         grant_type: 'password',
-        username: 'obada@admin.com', // 'api@example.com',
-        password: '1234' //  password: 'sylius-api'
+        username: 'api@example.com' ,//'obada@admin.com', // 'api@example.com',
+        password: 'sylius-api' //'1234' //  password: 'sylius-api'
   
       } 
       const header = {
         'Accept': 'application/json',
         'Authorization': 'Bearer ' + ''
       }  
-        this.$http.post('http://bknd.alarabexpress.com/api/oauth/v2/token',data).then(response =>{
-            this.TestArray = response.body 
-        })
+      var proxy = 'https://cors-anywhere.herokuapp.com/';
+      this.$http.get('http://bknd.alarabexpress.com/api/oauth/v2/token',{params: {client_id:'demo_client',client_secret:'secret_demo_client',grant_type:'password',username:'api@example.com',password: 'sylius-api'}}).then(response =>{
+             this.TestArray = response.body 
+         })
+      // var myUrl ='http://bknd.alarabexpress.com/api/oauth/v2/token' ;
+      // var finalURL = proxy + myUrl;
+      //   this.$http.get('http://bknd.alarabexpress.com/api/oauth/v2/token',data).then(response =>{
+      //       this.TestArray = response.body 
+      //   })
   }
   }
 </script>
